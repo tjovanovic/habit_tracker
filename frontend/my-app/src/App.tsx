ca607db3732ebb8000bb8a7bedb5ab1e2eaa5ff6
Tomislav Jovanovic <tjovanovic994@gmail.com>
2024-09-26 11:15:00 +0100

    frontend: more changes

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState("El blanco vido...")
+
+  useEffect(() => {
+    const url = "/api";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, { method: "POST" });
+        const json = await response.text();
+        console.log(json);
+        setState(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Test -- {state}
     </div>
   );
 }
