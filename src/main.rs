432eb16ed13b1e2678124515565e85227f6ccdb9
Tomislav Jovanovic <tjovanovic994@gmail.com>
2024-09-26 07:57:10 +0100

    more code

--- src/main.rs
+++ src/main.rs
@@ -60,24 +60,3 @@ async fn main() {
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
-
-// we can also write a custom extractor that grabs a connection from the pool
-// which setup is appropriate depends on your application
-struct DatabaseConnection(sqlx::pool::PoolConnection<sqlx::Postgres>);
-
-#[async_trait]
-impl<S> FromRequestParts<S> for DatabaseConnection
-where
-    PgPool: FromRef<S>,
-    S: Send + Sync,
-{
-    type Rejection = (StatusCode, String);
-
-    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let pool = PgPool::from_ref(state);
-
-        let conn = pool.acquire().await.map_err(root::internal_error)?;
-
-        Ok(Self(conn))
-    }
-}
--- src/routes/root.rs
+++ src/routes/root.rs
@@ -1,6 +1,11 @@
-use crate::core::habits::{Habit, HabitType, WeekDay};
-use axum::{extract::State, http::StatusCode};
+use axum::{
+    extract::{Query, State},
+    http::StatusCode,
+};
 use sqlx::postgres::PgPool;
+use std::collections::HashMap;
+
+use crate::core::habits::{Habit, HabitType, WeekDay};
 
 // we can extract the connection pool with `State`
 pub async fn get(State(pool): State<PgPool>) -> Result<String, (StatusCode, String)> {
@@ -10,17 +15,25 @@ pub async fn get(State(pool): State<PgPool>) -> Result<String, (StatusCode, Stri
         .map_err(internal_error)
 }
 
-pub async fn post(State(pool): State<PgPool>) -> Result<String, (StatusCode, String)> {
+pub async fn post(
+    State(pool): State<PgPool>,
+    Query(params): Query<HashMap<String, String>>,
+    body: String,
+) -> Result<String, (StatusCode, String)> {
     let result = Habit::new(
         String::from("Test"),
         vec![WeekDay::Monday, WeekDay::Tuesday],
         HabitType::Completed,
     );
+    println!("{}", body);
+    for (key, val) in params.into_iter() {
+        println!("{} / {}", key, val);
+    }
     sqlx::query("insert into habits (name) values ($1)")
         .bind(result.name)
         .execute(&pool)
         .await
-        .map(internal_success)
+        .map(|_s| String::from("Ok"))
         .map_err(internal_error)
 
     // Ok(String::from("Accepted"))
@@ -34,16 +47,3 @@ where
 {
     (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
-
-pub fn internal_success<T>(succ: T) -> String {
-    String::from("X")
-}
-
-// async fn using_connection_extractor(
-//     DatabaseConnection(mut conn): DatabaseConnection,
-// ) -> Result<String, (StatusCode, String)> {
-//     sqlx::query_scalar("select 'hello world from pg'")
-//         .fetch_one(&mut *conn)
-//         .await
-//         .map_err(internal_error)
-// }
