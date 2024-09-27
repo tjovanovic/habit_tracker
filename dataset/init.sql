c55cb1ee5afa4c3824d837baf08836b3fe048830
Tomislav Jovanovic <tjovanovic994@gmail.com>
2024-09-27 11:27:06 +0100

    dc: update table defs

--- dataset/init.sql
+++ dataset/init.sql
@@ -1,3 +1,17 @@
+create table users (
+    id integer primary key,
+    name varchar,
+    password varchar,
+    created_at timestamptz default now()
+);
+
 create table habits (
-    name varchar
+    id integer primary key,
+    name varchar,
+    desired_week_days json,
+    completed_week_days json default null,
+    habit_type json,
+    category varchar,
+    priority smallint,
+    user_id integer references users(id)
 );
